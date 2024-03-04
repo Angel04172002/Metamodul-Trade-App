2eb9af5f20b96294c21a15264452d7e87fef0267
Angel Iliev <ailievengineer@gmail.com>
2024-03-04 17:38:51 +0200

    Added simple views and view models

---
+++ MetamodulTradeApp.Core/Models/Post/AllPostsViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetamodulTradeApp.Core.Models.Post
+{
+    public class AllPostsViewModel
+    {
+
+    }
+}
