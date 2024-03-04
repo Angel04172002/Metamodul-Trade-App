181ec22ca521bb91d94199bbdc388ba4711fdcda
Angel Iliev <ailievengineer@gmail.com>
2024-03-04 17:39:51 +0200

    Added methods in Post Controller

---
+++ MetamodulTradeApp/Controllers/PostController.cs
@@ -0,0 +1,53 @@
+﻿using MetamodulTradeApp.Core.Models.Post;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MetamodulTradeApp.Controllers
+{
+    [Authorize]
+    public class PostController : Controller
+    {
+        //Return view with all posts
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(PostFormViewModel model)
+        {
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpGet]   
+        public async Task<IActionResult> Edit(int id)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(PostFormViewModel model, int id)
+        {
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Remove()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Remove(int id)
+        {
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
