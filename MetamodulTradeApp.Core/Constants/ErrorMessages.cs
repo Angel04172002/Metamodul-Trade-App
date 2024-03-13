49edb7c232cba00fc60880890ea6fbcfa18e0ca4
Angel <Angel@DESKTOP-73NFD1L>
2024-03-13 23:59:44 +0200

    Added error messages

---
+++ MetamodulTradeApp.Core/Constants/ErrorMessages.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetamodulTradeApp.Core.Constants
+{
+    public static class ErrorMessages
+    {
+        public const string RequireErrorMessage = "Field {0} is required!";
+        public const string StringLengthErrorMessage = "Field {0} must be between {2} and {1} characters long!";
+    }
+}
